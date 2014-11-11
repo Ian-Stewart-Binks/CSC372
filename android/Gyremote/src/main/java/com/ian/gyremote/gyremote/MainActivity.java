a97519dc076350e9dda0ec70bf0116f005989847
Ian-Stewart-Binks <istewartbinks@gmail.com>
2014-11-11 00:12:05 -0500

    Android: Brake ane forward implemented.

--- android/Gyremote/src/main/java/com/ian/gyremote/gyremote/MainActivity.java
+++ android/Gyremote/src/main/java/com/ian/gyremote/gyremote/MainActivity.java
@@ -1,19 +1,108 @@
 package com.ian.gyremote.gyremote;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
-public class MainActivity extends ActionBarActivity {
+import android.os.Bundle;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private Button On,Off,Visible,list;
+    private Set<BluetoothDevice> pairedDevices;
+    private ListView lv;
+    private static final String TAG = "bluetooth";
+
+
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
+
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    private static String address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        On = (Button)findViewById(R.id.button1);
+        Off = (Button)findViewById(R.id.button2);
+        Visible = (Button)findViewById(R.id.button3);
+        list = (Button)findViewById(R.id.button4);
+        lv = (ListView)findViewById(R.id.listView);
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 
+    public void on(View view){
+        if (!btAdapter.isEnabled()) {
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
+            Toast.makeText(getApplicationContext(),"Turned on", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Already on", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void list(View view){
+        pairedDevices = btAdapter.getBondedDevices();
+
+        ArrayList list = new ArrayList();
+        for(BluetoothDevice dev : pairedDevices) {
+            list.add(dev.getName());
+            address = dev.getAddress();
+        }
+
+        Toast.makeText(getApplicationContext(),"Showing Paired Devices",
+                Toast.LENGTH_SHORT).show();
+
+        final ArrayAdapter adapter = new ArrayAdapter
+                (this,android.R.layout.simple_list_item_1, list);
+
+        lv.setAdapter(adapter);
+
+    }
+
+    public void off(View view){
+        btAdapter.disable();
+        Toast.makeText(getApplicationContext(),"Turned off" ,
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void goForward(View view){
+        sendData("g");
+    }
+
+    public void brake(View view){
+        sendData("r");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -23,21 +112,77 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    public void onResume() {
+        super.onResume();
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        try {
+            btSocket = createBluetoothSocket(device);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return super.onOptionsItemSelected(item);
+
+        btAdapter.cancelDiscovery();
+
+        try {
+            btSocket.connect();
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
+        }
+
+        try {
+            outStream = btSocket.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    /**
-     * Turns on an LED.
-     */
-    public void turnLEDOn() {
-        
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        if(Build.VERSION.SDK_INT >= 10){
+            try {
+                final Method  m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", new Class[] { UUID.class });
+                return (BluetoothSocket) m.invoke(device, MY_UUID);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return  device.createRfcommSocketToServiceRecord(MY_UUID);
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (outStream != null) {
+            try {
+                outStream.flush();
+            } catch (IOException e) {
+                Toast.makeText(this, "Error in outStream.flush()", Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+        }
+
+        try     {
+            btSocket.close();
+        } catch (IOException e2) {
+            e2.printStackTrace();
+        }
+    }
+
+    private void sendData(String message) {
+        byte[] msgBuffer = message.getBytes();
+
+        Log.d(TAG, address + "" + message);
+
+        try {
+            outStream.write(msgBuffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
