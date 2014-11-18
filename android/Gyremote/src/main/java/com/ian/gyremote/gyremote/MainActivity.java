305985b33049c1d37a5b55fe17e2f1c229a53b97
Ian-Stewart-Binks <istewartbinks@gmail.com>
2014-11-18 18:52:26 -0500

    Android: Working for one sensor, still reading in val.

--- android/Gyremote/src/main/java/com/ian/gyremote/gyremote/MainActivity.java
+++ android/Gyremote/src/main/java/com/ian/gyremote/gyremote/MainActivity.java
@@ -6,14 +6,14 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Build;
+import android.os.*;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -32,14 +32,25 @@ import android.widget.*;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
-    private Button On,Off,Visible,list;
+    private Button something, forward, reverse, list, brake, right, left;
     private Set<BluetoothDevice> pairedDevices;
     private static final String TAG = "bluetooth";
     private SensorManager sManager;
     private TextView tv;
+    public static final int MESSAGE_READ = 1;
+    private Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            final int what = msg.what;
+            switch(what) {
+                case MESSAGE_READ: tv.setText(msg.toString()); break;
+            }
+        }
+    };
 
     private BluetoothAdapter btAdapter = null;
     private BluetoothSocket btSocket = null;
+
+    private InputStream inputStream = null;
 
     // Used to write to the bluetooth stream.
     private OutputStream outStream = null;
@@ -54,14 +65,53 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        On = (Button)findViewById(R.id.button1);
-        Off = (Button)findViewById(R.id.button2);
-        Visible = (Button)findViewById(R.id.button3);
+        something = (Button)findViewById(R.id.button1);
+        forward = (Button)findViewById(R.id.button2);
+        reverse = (Button)findViewById(R.id.button3);
         list = (Button)findViewById(R.id.button4);
+        brake = (Button)findViewById(R.id.button5);
+        right = (Button)findViewById(R.id.button6);
+        left = (Button)findViewById(R.id.button);
 
         tv = (TextView)findViewById(R.id.textView3);
 
         btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+
+        new Thread (new Runnable() {
+            @Override
+            public void run() {
+                final byte[] buffer = new byte[1024];
+                int bytes; // bytes returned from read()
+                while (true) {
+
+                    try {
+                        // Read from the InputStream
+                        if (inputStream != null && inputStream.available() > 0) {
+                            bytes = inputStream.read(buffer);
+                            Log.d(TAG, buffer.toString());
+                            mHandler.post(new Runnable()
+                            {
+                                public void run()
+                                {
+                                    try {
+
+                                        String decoded = new String(buffer, "UTF-8");
+                                        tv.setText(decoded);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
+
+                        }
+                        // Send the obtained bytes to the UI activity
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 
     public void on(View view){
@@ -93,6 +143,10 @@ public class MainActivity extends Activity implements SensorEventListener {
                 Toast.LENGTH_LONG).show();
     }
 
+    public void readData() {
+
+    }
+
     public void goForward(View view){
         sendData("g");
     }
@@ -118,6 +172,26 @@ public class MainActivity extends Activity implements SensorEventListener {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    public void run2() {
+        byte[] buffer = new byte[1024];  // buffer store for the stream
+        int bytes; // bytes returned from read()
+
+        // Keep listening to the InputStream until an exception occurs
+        while (true) {
+            try {
+                // Read from the InputStream
+                if (inputStream != null) {
+                    bytes = inputStream.read(buffer);
+                    tv.setText(bytes+"");
+
+                }
+                // Send the obtained bytes to the UI activity
+            } catch (IOException e) {
+                break;
+            }
+        }
     }
 
     @Override
@@ -152,6 +226,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 
         try {
             outStream = btSocket.getOutputStream();
+            inputStream = btSocket.getInputStream();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -177,7 +252,6 @@ public class MainActivity extends Activity implements SensorEventListener {
             try {
                 outStream.flush();
             } catch (IOException e) {
-                Toast.makeText(this, "Error in outStream.flush()", Toast.LENGTH_LONG).show();
                 e.printStackTrace();
             }
         }
@@ -190,6 +264,10 @@ public class MainActivity extends Activity implements SensorEventListener {
     }
 
     private void sendData(String message) {
+        if (message == currentChar) {
+            return;
+        }
+        currentChar = message;
         byte[] msgBuffer = message.getBytes();
 
         Log.d(TAG, address + "" + message);
@@ -203,38 +281,56 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     int currentRoll = 0;
     int currentPitch = 0;
+    String currentChar = "";
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-
-        // When on flat surface, values are:
-        // ROLL:   ~0 brake then -90 is 255
-        // PITCH:  ~0
-        if (event.values[1] < 10 && event.values[1] > -10) {
-            if (event.values[2] > 25) { sendData("g"); } // Forward (fast)
-            else if (event.values[2] > 0) { sendData("h"); } // Forward (slow)
-            else if (event.values[2] < 0 && event.values[2] > -1) { sendData("b"); }  // Brake
-            else if (event.values[2] < -25) { sendData("r"); } // Reverse (fast)
-            else if (event.values[2] < 0) { sendData("p"); } // Reverse (slow)
-
-        } else if (event.values[1] < 25 && event.values[1] > 10) { // Left Slow
-            if (event.values[2] > 25) { sendData("a"); }  // Forward (fast)
-            else if (event.values[2] > 0) { sendData("e"); }  // Forward (slow)
-            else if (event.values[2] < 0 && event.values[2] > -1) { sendData("b"); } // Brake
-            else if (event.values[2] < -25) { sendData("c"); } // Reverse (fast)
-            else if (event.values[2] < 0) { sendData("d"); } // Reverse (slow)
-
-        } else if (event.values[1] > -25 && event.values[1] < -10) { // Right Slow
-            if (event.values[2] > 25) { sendData("u"); } // Forward (fast)
-            else if (event.values[2] > 0) { sendData("x"); }  // Forward (slow)
-            else if (event.values[2] < 0 && event.values[2] > -1) { sendData("b"); } // Brake
-            else if (event.values[2] < -25) { sendData("y"); } // Reverse (fast)
-            else if (event.values[2] < 0) { sendData("z"); } // Reverse (slow)
-
-        }
-        tv.setText("Orientation X (Roll) :"+ Float.toString(event.values[2]) +"\n"+
-                "Orientation Y (Pitch) :"+ Float.toString(event.values[1]) +"\n"+
-                "Orientation Z (Yaw) :"+ Float.toString(event.values[0]));
+//
+//        // When on flat surface, values are:
+//        // ROLL:   ~0 brake then -90 is 255
+//        // PITCH:  ~0
+//
+//        // PROTOCOL:
+//        // STRAIGHT:
+//        // g: FORWARD FAST
+//        // h: FORWARD SLOW
+//        // r: REVERSE FAST
+//        // p: REVERSE SLOW
+//        // LEFT:
+//        // a: FORWARD FAST
+//        // e: FORWARD SLOW
+//        // c: REVERSE FAST
+//        // d: REVERSE SLOW
+//        // RIGHT:
+//        // u: FORWARD FAST
+//        // x: FORWARD SLOW
+//        // y: REVERSE FAST
+//        // z: REVERSE SLOW
+//        if (event.values[1] < 10 && event.values[1] > -10) {
+//            if (event.values[2] > 25) { sendData("g"); } // Forward (fast)
+//            else if (event.values[2] > 0) { sendData("h"); } // Forward (slow)
+//            else if (event.values[2] < 0 && event.values[2] > -1) { sendData("b"); }  // Brake
+//            else if (event.values[2] < -25) { sendData("r"); } // Reverse (fast)
+//            else if (event.values[2] < 0) { sendData("p"); } // Reverse (slow)
+//
+//        } else if (event.values[1] < 25 && event.values[1] > 10) { // Left Slow
+//            if (event.values[2] > 25) { sendData("a"); }  // Forward (fast)
+//            else if (event.values[2] > 0) { sendData("e"); }  // Forward (slow)
+//            else if (event.values[2] < 0 && event.values[2] > -1) { sendData("b"); } // Brake
+//            else if (event.values[2] < -25) { sendData("c"); } // Reverse (fast)
+//            else if (event.values[2] < 0) { sendData("d"); } // Reverse (slow)
+//
+//        } else if (event.values[1] > -25 && event.values[1] < -10) { // Right Slow
+//            if (event.values[2] > 25) { sendData("u"); } // Forward (fast)
+//            else if (event.values[2] > 0) { sendData("x"); }  // Forward (slow)
+//            else if (event.values[2] < 0 && event.values[2] > -1) { sendData("b"); } // Brake
+//            else if (event.values[2] < -25) { sendData("y"); } // Reverse (fast)
+//            else if (event.values[2] < 0) { sendData("z"); } // Reverse (slow)
+//
+//        }
+//        tv.setText("Orientation X (Roll) :"+ Float.toString(event.values[2]) +"\n"+
+//                "Orientation Y (Pitch) :"+ Float.toString(event.values[1]) +"\n"+
+//                "Orientation Z (Yaw) :"+ Float.toString(event.values[0]));
     }
 
     @Override
