b0108aef81e88a2b5893c6868bc78558c83240ab
Ian Stewart-Binks <istewartbinks@gmail.com>
2015-02-21 00:59:32 -0500

    Android: Clean up android.

--- android/Gyremote/src/main/java/com/ian/gyremote/gyremote/MainActivity.java
+++ android/Gyremote/src/main/java/com/ian/gyremote/gyremote/MainActivity.java
@@ -7,39 +7,57 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.*;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
 import android.widget.*;
 import android.graphics.Color;
 
+/**
+ *
+ */
 public class MainActivity extends Activity implements SensorEventListener {
 
-    private Button something, forward, reverse, list, brake, right, left, light;
+    /**
+     *
+     */
     private ImageView frontIndicator, backIndicator, leftIndicator, rightIndicator;
-    private Set<BluetoothDevice> pairedDevices;
+
+    /**
+     *
+     */
     private static final String TAG = "bluetooth";
+
+    /**
+     *
+     */
     private SensorManager sManager;
+
+    /**
+     *
+     */
     private TextView tv;
-    public static final int MESSAGE_READ = 1;
+
+    /**
+     *
+     */
+    private static final int MESSAGE_READ = 1;
+
+    /**
+     *
+     */
     private Handler mHandler = new Handler() {
         public void handleMessage(Message msg) {
             final int what = msg.what;
@@ -49,13 +67,29 @@ public class MainActivity extends Activity implements SensorEventListener {
         }
     };
 
+    /**
+     *
+     */
     private BluetoothAdapter btAdapter = null;
+
+    /**
+     *
+     */
     private BluetoothSocket btSocket = null;
 
+    /**
+     *
+     */
     private InputStream inputStream = null;
 
+    /**
+     *
+     */
     private OutputStream outStream = null;
 
+    /**
+     *
+     */
     private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     // MAC Address of bluetooth shield.
@@ -66,12 +100,6 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         frontIndicator = (ImageView)findViewById(R.id.imageView3);
-        something = (Button)findViewById(R.id.button1);
-        forward = (Button)findViewById(R.id.button2);
-        reverse = (Button)findViewById(R.id.button3);
-        list = (Button)findViewById(R.id.button4);
-        brake = (Button)findViewById(R.id.button5);
-        light = (Button)findViewById(R.id.button7);
         rightIndicator = (ImageView)findViewById(R.id.imageView2);
         leftIndicator = (ImageView)findViewById(R.id.imageView4);
         backIndicator = (ImageView)findViewById(R.id.imageView);
@@ -84,43 +112,46 @@ public class MainActivity extends Activity implements SensorEventListener {
             @Override
             public void run() {
                 final byte[] buffer = new byte[1024];
-                int bytes;
 
                 while (true) {
-
                     try {
                         if (inputStream != null && inputStream.available() > 0) {
-                            bytes = inputStream.read(buffer);
-                            Log.d(TAG, buffer.toString());
                             mHandler.post(new Runnable() {
                                 public void run() {
                                     try {
                                         String decoded = new String(buffer, "UTF-8");
-                                        if (decoded.charAt(0) == '0') {
-                                            frontIndicator.setBackgroundColor(Color.BLUE);
-                                        } else if (decoded.charAt(0) == '1') {
-                                            frontIndicator.setBackgroundColor(Color.RED);
-                                        } else if (decoded.charAt(0) == '2') {
-                                            backIndicator.setBackgroundColor(Color.BLUE);
-                                        } else if (decoded.charAt(0) == '3') {
-                                            backIndicator.setBackgroundColor(Color.RED);
-                                        } else if (decoded.charAt(0) == '4') {
-                                            rightIndicator.setBackgroundColor(Color.BLUE);
-                                        } else if (decoded.charAt(0) == '5') {
-                                            rightIndicator.setBackgroundColor(Color.RED);
-                                        } else if (decoded.charAt(0) == '6') {
-                                            leftIndicator.setBackgroundColor(Color.BLUE);
-                                        } else if (decoded.charAt(0) == '7') {
-                                            leftIndicator.setBackgroundColor(Color.RED);
+                                        switch (decoded.charAt(0)) {
+                                            case '0':
+                                                frontIndicator.setBackgroundColor(Color.BLUE);
+                                                break;
+                                            case '1':
+                                                frontIndicator.setBackgroundColor(Color.RED);
+                                                break;
+                                            case '2':
+                                                backIndicator.setBackgroundColor(Color.BLUE);
+                                                break;
+                                            case '3':
+                                                backIndicator.setBackgroundColor(Color.RED);
+                                                break;
+                                            case '4':
+                                                rightIndicator.setBackgroundColor(Color.BLUE);
+                                                break;
+                                            case '5':
+                                                rightIndicator.setBackgroundColor(Color.RED);
+                                                break;
+                                            case '6':
+                                                leftIndicator.setBackgroundColor(Color.BLUE);
+                                                break;
+                                            case '7':
+                                                leftIndicator.setBackgroundColor(Color.RED);
+                                                break;
                                         }
                                     } catch (Exception e) {
                                         e.printStackTrace();
                                     }
                                 }
                             });
-
                         }
-                        
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -129,19 +160,37 @@ public class MainActivity extends Activity implements SensorEventListener {
         }).start();
     }
 
-    public void on(View view){
+    /**
+     *
+     * @param view
+     */
+    public void on(View view) {
         sendData("A");
     }
 
+    /**
+     *
+     * @param view
+     */
     public void lightsOn(View view) {
         sendData("o");
     }
 
+    /**
+     *
+     * @param view
+     */
     public void disable(View view) {
         sendData("9");
     }
 
-    public void list(View view){
+    /**
+     *
+     * @param view
+     */
+    public void list(View view) {
+        Set<BluetoothDevice> pairedDevices;
+
         pairedDevices = btAdapter.getBondedDevices();
 
         ArrayList list = new ArrayList();
@@ -154,52 +203,62 @@ public class MainActivity extends Activity implements SensorEventListener {
                 Toast.LENGTH_SHORT).show();
     }
 
-    public void off(View view){
+    /**
+     *
+     * @param view
+     */
+    void off(View view) {
         btAdapter.disable();
         Toast.makeText(getApplicationContext(),"Turned off" ,
                 Toast.LENGTH_LONG).show();
     }
 
-    public void goForward(){
+    /**
+     *
+     */
+    void goForward(){
         sendData("g");
     }
 
-    public void brake(View view){
+    /**
+     *
+     * @param view
+     */
+    void brake(View view){
         sendData("b");
     }
 
-    public void reverse() {
+    /**
+     *
+     */
+    void reverse() {
         sendData("r");
     }
 
-    public void goLeft() {
+    /**
+     *
+     */
+    void goLeft() {
         sendData("l");
     }
 
-    public void goRight() {
+    /**
+     *
+     */
+    void goRight() {
         sendData("i");
     }
 
+    /**
+     *
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
-
-    public void run2() {
-        byte[] buffer = new byte[1024];
-        int bytes; 
-        while (true) {
-            try {
-                if (inputStream != null) {
-                    bytes = inputStream.read(buffer);
-                    tv.setText(bytes + "");
-                }
-            } catch (IOException e) {
-                break;
-            }
-        }
     }
 
     @Override
@@ -240,6 +299,12 @@ public class MainActivity extends Activity implements SensorEventListener {
         }
     }
 
+    /**
+     *
+     * @param device
+     * @return
+     * @throws IOException
+     */
     private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
 
         try {
@@ -264,15 +329,18 @@ public class MainActivity extends Activity implements SensorEventListener {
             }
         }
 
-        try     {
+        try {
             btSocket.close();
         } catch (IOException e2) {
             e2.printStackTrace();
         }
     }
 
+    /**
+     *
+     * @param message
+     */
     private void sendData(String message) {
-        currentChar = message;
         byte[] msgBuffer = message.getBytes();
 
         Log.d(TAG, address + "" + message);
@@ -284,15 +352,9 @@ public class MainActivity extends Activity implements SensorEventListener {
         }
     }
 
-    int currentRoll = 0;
-    int currentPitch = 0;
-    String currentChar = "";
-
     @Override
     public void onSensorChanged(SensorEvent event) {
-        char memoryVal = '0';
         tv.setText("" + (event.values[2]));
-
 
         if (event.values[2] > 30) {
             goLeft();
